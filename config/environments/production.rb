fb36af184c88bc8f510c0912e69651f9c3308b0d
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-20 06:05:44 +0000

    test committt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host:  'https://aqueous-spire-90539.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  # ここに環境変数をuser_name,passwordとして使用することが記されている
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: "heroku.com",
+  address: "smtp.SendGrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 end
